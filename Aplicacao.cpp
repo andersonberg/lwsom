33dd596d7ca967378f1dadfa8f53a30ecef855df
Anderson Berg <andersonberg@gmail.com>
2013-05-25 18:41:44 -0300

    leitura do arquivo

--- Aplicacao.cpp
+++ Aplicacao.cpp
@@ -8,6 +8,12 @@
 #include "Aplicacao.h"
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sstream>
+#include <string>
+
+using namespace std;
 
 Aplicacao::Aplicacao() {
 
@@ -16,22 +22,40 @@ Aplicacao::Aplicacao() {
 void Aplicacao::leitor(){
 	ifstream arquivo;
 	arquivo.open("iris.data");
-	char matriz[1500];
+	char matriz_linha[5000];
+	float matriz[150][5000];
+	int i = 0;
+	unsigned int j = 0;
+
 	cout << "Iniciando...\n";
 
 	if(!arquivo.is_open()){
 		cout << "Nao foi possivel abrir o arquivo.\n";
 		arquivo.clear();
 	}
-	else{
-		string str;
-		arquivo >> str;
-		cout << str << endl;
-	}
 
-	while(arquivo.getline(matriz, 1500)){
-		cout << "Lendo arquivo...\n";
-		cout << matriz << "\n";
+	while(arquivo.getline(matriz_linha, 5000)){
+		string linha = matriz_linha;
+		istringstream iss(linha);
+
+		do{
+			string sub;
+			iss >> sub;
+			cout << sub << endl;
+		}while (iss);
+
+		char a[1];
+		a[0] = '#';
+		if (matriz_linha[0] == a[0]){
+			cout << "\nIniciando nova matriz...\n";
+			i = 0;
+		}
+		else{
+			cout << "Linha: " << i << endl;
+
+		}
+		cout << matriz_linha << "\n";
+		i++;
 	}
 	arquivo.close();
 }
@@ -39,4 +63,3 @@ void Aplicacao::leitor(){
 Aplicacao::~Aplicacao() {
 	// TODO Auto-generated destructor stub
 }
-
